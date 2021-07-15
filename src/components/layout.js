8637ad873bef3e3d546c7400e05c68952d6ebe93
hiroki_sadahisa <sadahisa@kairos.co.jp>
2021-07-15 11:23:09 +0900

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-hello-world.git)

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../styles/layout.scss"
+
+export default function Home() {
+  return (
+    <div className="layout">
+      <main>{children}</main>
+    </div>
+  )
+}
